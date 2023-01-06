09e2c8153abd170074d4708b72e4379821ead1c7
Racque Cordeta <66395124+Ghostcoder0427@users.noreply.github.com>
2023-01-05 16:55:17 -0800

    Create SeniorCustomer.java

---
+++ SeniorCustomer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package final_exam;
+
+/**
+ *
+ * @author Best Friend
+ */
+
+public class SeniorCustomer extends Customer {
+	public static final double DISCOUNT = 0.20;
+
+	public SeniorCustomer(String name, double amount) {
+		super(name, amount);
+                
+	}
+	
+        @Override
+	public double calculateBill() {
+		return (amount-(amount * DISCOUNT));
+	}
+}
